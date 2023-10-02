eb346beb69315321e290a537742e9651dcc08ae1
Ronja <ronjasjogren@hotmail.com>
2023-10-02 21:02:51 +0200

    add introduce, getemail and getname methods

--- Assignment2/FunFeatures.cs
+++ Assignment2/FunFeatures.cs
@@ -13,16 +13,28 @@ namespace Assignment2
 
         public void Start()
         {
+            // Welcome the player and get some user information
+            Introduce();
+
             // Start method
+            //PredictMyDay();
+            //CalculateStrengthLength();
+            //repeat = RunAgain();
         }
         private bool RunAgain()
         {
-            // Method to decide i the code should run again or not
+            // Method to decide if the code should run again or not
             return false;
         }
         private void Introduce()
         {
-            // Welcome the player
+            // Welcome the player and get some user information
+            Console.WriteLine("Hello! My name is Ronja and I am a student of the 2023 autumn semester\n");
+            Console.WriteLine("Let me know about yourself!");
+            ReadName();
+            ReadEmail();
+            Console.WriteLine("\nNice to meet you " + name);
+            Console.WriteLine("Your email is " + email);
         }
         private void PredictMyDay()
         {
@@ -35,10 +47,17 @@ namespace Assignment2
         private void ReadEmail()
         {
             // Read the email from the user
+            Console.Write("Your email please: ");
+            email = Console.ReadLine();
         }
         private void ReadName()
         {
             // Read the name from the user
+            Console.Write("Your first name please: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Your last name please: ");
+            string lastName = Console.ReadLine().ToUpper();
+            name = lastName + ", " + firstName;
         }
     }
 }
--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -3,6 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        FunFeatures funFeatures = new FunFeatures();
+        funFeatures.Start();
     }
 }
