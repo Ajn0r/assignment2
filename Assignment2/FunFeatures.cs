21a795f7fd324b088c79c8df1e321dad09464fac
Ronja <ronjasjogren@hotmail.com>
2023-10-02 20:48:39 +0200

    add FunFeatures base methods

--- Assignment2/FunFeatures.cs
+++ Assignment2/FunFeatures.cs
@@ -8,5 +8,37 @@ namespace Assignment2
 {
     internal class FunFeatures
     {
+        private string email;
+        private string name;
+
+        public void Start()
+        {
+            // Start method
+        }
+        private bool RunAgain()
+        {
+            // Method to decide i the code should run again or not
+            return false;
+        }
+        private void Introduce()
+        {
+            // Welcome the player
+        }
+        private void PredictMyDay()
+        {
+
+        }
+        private void CalculateStrengthLength()
+        {
+
+        }
+        private void ReadEmail()
+        {
+            // Read the email from the user
+        }
+        private void ReadName()
+        {
+            // Read the name from the user
+        }
     }
 }
